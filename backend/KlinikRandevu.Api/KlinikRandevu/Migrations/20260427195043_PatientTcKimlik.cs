8a6d90fe063af2c95e871afb81c2ceb243016807
Batuhan Kose <batuhankose36@gmail.com>
2026-04-27 22:54:44 +0300

    Patient Servis katmanı iş kuralları eklendi step 2 ve tc kimlik modele eklendi

---
+++ backend/KlinikRandevu.Api/KlinikRandevu/Migrations/20260427195043_PatientTcKimlik.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KlinikRandevu.Migrations
+{
+    /// <inheritdoc />
+    public partial class PatientTcKimlik : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "TcKimlik",
+                table: "Patients",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.UpdateData(
+                table: "Patients",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedAt", "TcKimlik" },
+                values: new object[] { new DateTime(2026, 4, 27, 22, 50, 43, 12, DateTimeKind.Local).AddTicks(9861), 0L });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TcKimlik",
+                table: "Patients");
+
+            migrationBuilder.UpdateData(
+                table: "Patients",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2026, 4, 27, 21, 59, 52, 159, DateTimeKind.Local).AddTicks(9898));
+        }
+    }
+}
